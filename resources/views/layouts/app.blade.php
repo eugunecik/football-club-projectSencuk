56c2a15167b78d248409883004ab298bfd2b74b2
Eugune Sencuk <zenasencuk2@gmail.com>
2024-10-03 20:05:39 +0300

    make better look

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,19 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Футбольний Клуб</title>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <title>Football Club Management</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
-        <a class="navbar-brand" href="/">Футбольний Клуб</a>
-        <div class="navbar-nav">
-            <a class="nav-item nav-link" href="{{ route('teams.index') }}">Команди</a>
-            <a class="nav-item nav-link" href="{{ route('players.index') }}">Гравці</a>
-            <a class="nav-item nav-link" href="{{ route('coaches.index') }}">Тренери</a>
-            <a class="nav-item nav-link" href="{{ route('matches.index') }}">Матчі</a>
-            <a class="nav-item nav-link" href="{{ route('stadiums.index') }}">Стадіони</a>
-            <a class="nav-item nav-link" href="{{ route('sponsors.index') }}">Спонсори</a>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-4">
+        <div class="container">
+            <a class="navbar-brand" href="/">Football Club</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link" href="{{ route('teams.index') }}">Teams</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('players.index') }}">Players</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('coaches.index') }}">Coaches</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('matches.index') }}">Matches</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('stadiums.index') }}">Stadiums</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('sponsors.index') }}">Sponsors</a></li>
+                </ul>
+            </div>
         </div>
     </nav>
 
@@ -23,8 +31,10 @@
         @yield('content')
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <footer class="mt-5 py-3 bg-light">
+        <div class="container text-center">
+            <span class="text-muted">© 2024 Football Club Management</span>
+        </div>
+    </footer>
 </body>
 </html>
