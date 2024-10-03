d73559b486f70ec506bdf4b874960dcbc68ce886
Eugune Sencuk <zenasencuk2@gmail.com>
2024-10-03 20:08:52 +0300

    change look of teams page

--- resources/views/teams/index.blade.php
+++ resources/views/teams/index.blade.php
@@ -1,34 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Teams</h1>
+    <h1 class="mb-4">Teams</h1>
     <a href="{{ route('teams.create') }}" class="btn btn-primary mb-3">Create New Team</a>
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>City</th>
-                <th>Founded</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($teams as $team)
+    
+    <div class="table-responsive">
+        <table class="table table-striped table-hover">
+            <thead class="table-dark">
                 <tr>
-                    <td>{{ $team->name }}</td>
-                    <td>{{ $team->city }}</td>
-                    <td>{{ $team->founded }}</td>
-                    <td>
-                        <a href="{{ route('teams.show', $team) }}" class="btn btn-sm btn-info">View</a>
-                        <a href="{{ route('teams.edit', $team) }}" class="btn btn-sm btn-primary">Edit</a>
-                        <form action="{{ route('teams.destroy', $team) }}" method="POST" style="display: inline-block;">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Are you sure?')">Delete</button>
-                        </form>
-                    </td>
+                    <th>Name</th>
+                    <th>City</th>
+                    <th>Founded</th>
+                    <th>Actions</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach($teams as $team)
+                    <tr>
+                        <td>{{ $team->name }}</td>
+                        <td>{{ $team->city }}</td>
+                        <td>{{ $team->founded }}</td>
+                        <td>
+                            <a href="{{ route('teams.show', $team) }}" class="btn btn-sm btn-info">View</a>
+                            <a href="{{ route('teams.edit', $team) }}" class="btn btn-sm btn-primary">Edit</a>
+                            <form action="{{ route('teams.destroy', $team) }}" method="POST" style="display: inline-block;">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Are you sure?')">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
