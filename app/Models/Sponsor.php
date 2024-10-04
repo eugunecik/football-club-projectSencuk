2ffee694637d2179222ee04f2930a83e18a51cbd
Eugune Sencuk <zenasencuk2@gmail.com>
2024-10-04 13:31:54 +0300

    FCS-4 upgrete sponsor

--- app/Models/Sponsor.php
+++ app/Models/Sponsor.php
@@ -4,15 +4,67 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Carbon\Carbon;
 
 class Sponsor extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
-    protected $fillable = ['name', 'industry'];
+    protected $fillable = ['name', 'industry', 'website', 'logo_path'];
 
+    protected $dates = ['deleted_at'];
+
+    /**
+     * Get the teams associated with the sponsor.
+     */
     public function teams()
     {
-        return $this->belongsToMany(Team::class)->withPivot('contract_start', 'contract_end')->withTimestamps();
+        return $this->belongsToMany(Team::class)
+                    ->withPivot('contract_start', 'contract_end', 'amount')
+                    ->withTimestamps();
+    }
+
+    /**
+     * Get active sponsorships.
+     */
+    public function activeTeams()
+    {
+        return $this->teams()
+                    ->wherePivot('contract_end', '>', Carbon::now());
+    }
+
+    /**
+     * Scope a query to only include sponsors of a specific industry.
+     */
+    public function scopeIndustry($query, $industry)
+    {
+        return $query->where('industry', $industry);
+    }
+
+    /**
+     * Get the total sponsorship amount.
+     */
+    public function getTotalSponsorshipAmount()
+    {
+        return $this->teams()->sum('amount');
+    }
+
+    /**
+     * Check if the sponsor has any active contracts.
+     */
+    public function hasActiveContracts()
+    {
+        return $this->teams()
+                    ->wherePivot('contract_end', '>', Carbon::now())
+                    ->exists();
+    }
+
+    /**
+     * Get the logo URL.
+     */
+    public function getLogoUrlAttribute()
+    {
+        return $this->logo_path ? asset('storage/' . $this->logo_path) : null;
     }
 }
